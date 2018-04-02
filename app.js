5209420f5e92f803393a4b4332a46415966bb110
Arunkumar Ramachandriya <arun@Arunkumars-MacBook-Pro.local>
2018-04-02 20:55:18 +0100

    Cleaned up for project start

--- app.js
+++ app.js
@@ -1,19 +1,9 @@
 console.log('Starting app.');
 
 const fs = require('fs');
-const os = require('os');
+
 const notes = require('./notes');
-
-var sum = notes.add(3, -13);
-
-console.log(`The sum of 3 & -13 is ${sum}`);
 
 notes.addNote();
 
-
-// var user = os.userInfo();
-
-// console.log(user);
-
-// fs.appendFile('greetings.txt', `Hello ${user.username}!`);
 
