2bdcc98321d7b2145c4ba27000b9fb4e52fb24f2
Arunkumar Ramachandriya <arun@Arunkumars-MacBook-Pro.local>
2018-04-03 18:07:39 +0100

    playground file added

---
+++ playground/json.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+const originalNote = {
+    title: 'Some title',
+    body: 'Some body'
+};
+
+const originalNoteString = JSON.stringify(originalNote);
+console.log(typeof(originalNoteString));
+
+fs.writeFileSync('notes.json', originalNoteString);
+
+const noteString = fs.readFileSync('notes.json');
+
+const note = JSON.parse(noteString);
+console.log(typeof note);
+console.log(note);
