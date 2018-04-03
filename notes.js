296af153137bc0ff5ccc48b34ad96880c6092a2a
Arunkumar Ramachandriya <arun@Arunkumars-MacBook-Pro.local>
2018-04-03 19:03:10 +0100

    refactoring into helper functions

--- notes.js
+++ notes.js
@@ -1,27 +1,32 @@
 const fs = require('fs');
 
+var readNotes = () => {
+    try {
+        var notesString = fs.readFileSync('notes-data.json');
+        return JSON.parse(notesString);
+    } catch (e) {
+        return [];
+    }
+};
+
+var saveNotes = (notes) => {
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+};
+
 var addNote = (title, body) => {
-    var notes = [];
+    var notes = readNotes();
     var note = {
         title,
         body
     };
-
-    try {
-        var notesString = fs.readFileSync('notes-data.json');
-        notes = JSON.parse(notesString);
-    } catch (e) {
-        console.log(`An error occurred, but ignoring: ${e}`);
-    }
-
     var duplicateNotes = notes.filter(n => n.title === title);
 
     if (duplicateNotes.length === 0) {
         notes.push(note);
-        fs.writeFileSync('notes-data.json', JSON.stringify(notes));
-    } else {
-        console.log('Title already exists');
-    }
+        saveNotes(notes);
+
+        return note;
+    } 
 };
 
 var getAll = () => {
